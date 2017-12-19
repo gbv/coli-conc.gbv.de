59cbf9a70b9d7e89c8eee5e24d22564443e78591
Jakob Voss <voss@gbv.de>
2017-12-19 10:55:51 +0100

    Update funding perioid

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ include "$BASE/header.php";
     Project coli-conc 
     <a href="http://gepris.dfg.de/gepris/projekt/276843344">is funded</a>
     by the German Research Foundation (DFG)
-    from December 2015 till November 2017.
+    from December 2015 till November 2018.
     An abbreviated version of our grant proposal has been published
     with DOI <a class="alert-link" href="http://dx.doi.org/10.5281/zenodo.28290">10.5281/zenodo.28290</a>.
     See the <a href="publications">publications section</a> for 
