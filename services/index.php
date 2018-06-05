85aa3897613027552050253fd4002fbf983cff4e
Jakob Voss <voss@gbv.de>
2018-06-05 15:34:31 +0200

    Include services page

---
+++ services/index.php
@@ -0,0 +1,17 @@
+<?php
+$BASE = '..';
+$TITLE = 'Services';
+include "$BASE/header.php";
+?>
+
+<p>
+ <ul>
+  <li><a href="../terminologies">KOS Registry</a></li>
+  <li><a href="../cocoda/">Mapping Tool (prototype)</a></li>
+  <li><a href="../concordances/">Concordances</a></li>
+  <li>APIs (documentation will follow)</a>
+ </ul>
+</p>
+
+<?php 
+include 'footer.php';
