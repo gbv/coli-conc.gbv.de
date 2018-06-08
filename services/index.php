2181b3683d4fda3938c22192d9944a1b7f4acb2d
Stefan Peters <stefandesu@exo.pm>
2018-06-08 08:22:23 +0200

    Update services page

--- services/index.php
+++ services/index.php
@@ -6,12 +6,16 @@ include "$BASE/header.php";
 
 <p>
  <ul>
-  <li><a href="../terminologies">KOS Registry</a></li>
-  <li><a href="../cocoda/">Mapping Tool (prototype)</a></li>
-  <li><a href="../concordances/">Concordances</a></li>
-  <li>APIs (documentation will follow)</a>
+  <li><a href="..//publications/kostypes/">KOS Types</a></li>
+  <li><a href="../terminologies/">KOS Registry</a></li>
+  <li><a href="../concordances/">Concordance Registry</a></li>
+  <li><a href="../concordances/wikidata/">Wikidata Mappings</a></li>
+  <li><a href="../concordances/gnd/">GND Mappings</a></li>
+  <li><a href="../cocoda/">Cocoda Mapping Tool</a></li>
+  <li>Catalogue Oc-Occurrences (documentation will follow)</a>
+  <li>JSKOS API (documentation will follow)</a>
  </ul>
 </p>
 
-<?php 
+<?php
 include 'footer.php';
