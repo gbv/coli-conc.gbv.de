01cfe4cfe9af028ef587a6a07a9c6de8d498d89e
Jakob Voss <voss@gbv.de>
2019-01-03 11:28:03 +0100

    Move cocoda app to subdirectory

---
+++ cocoda/index.php
@@ -0,0 +1,26 @@
+<?php
+$BASE = '..';
+$TITLE = 'Cocoda';
+$FORMAT = 'markdown';
+include "$BASE/header.php";
+?>
+
+Cocoda is a web application to manage and create mappings between knowledge organization systems, such as classifications, authority files, and thesauri. See [cocoda documentation](https://gbv.github.io/cocoda/) for details and [git repository](https://github.com/gbv/cocoda) for source code.
+
+### Use Cocoda mapping tool
+
+* latest **[release version](app)**
+* current [development version](https://gbv.github.io/cocoda/dev/)
+
+### Get started
+
+*We highly recommend to just try out the [release version](app) and start mapping!*
+
+The [cocoda manual](https://gbv.github.io/cocoda/#manual) gives a short introduction.
+
+We also prepared some screencasts (in German):
+
+* [Mapping mit Cocoda 0.6.0](https://vimeo.com/307653028) (5 minutes, 2018-12-21)
+* [Kurzeinführung Mapping mit Cocoda 0.5.1](https://vimeo.com/296616305) (3 minutes, 2018-10-23)
+
+<!-- See also <https://github.com/gbv/cocoda/issues/237> to improve documentation!  -->
