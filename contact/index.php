33ce483236d214b4914b32eb6a388d68ab2eaf92
Dennis Tobola <Dennis.Tobola@gbv.de>
2019-06-06 10:19:31 +0200

    Change format to Markdown (contact/index.php)

--- contact/index.php
+++ contact/index.php
@@ -1,41 +1,33 @@
 <?php
 $BASE = '..';
+$FORMAT = 'markdown';
 include "$BASE/header.php";
 ?>
 
-<div class="row">
-  <div class="col-md-6">
-    <h3>Team</h3>
-    <p>
-    <dl>
-    <dt>General Contact</dt>
-        <dd>coli-conc@gbv.de</dd>
-        <dt>Project Coordination</dt>
-        <dd>U. Balakrishnan (balakrishnan@gbv.de)</dd>
-        <dt>Technical Coordination</dt>
-        <dd>Dr. Jakob Voß (voss@gbv.de)</dd>
-        <dt>Software Development</dt>
-        <dd>Stefan Peters (peters@gbv.de)</dd>
-        <dt>Student Assistant</dt>
-        <dd>Dennis Tobola</dd>
-    </dl>
-    </p>
-  </div>
-  <div class="col-md-6">
-    <h3>News</h3>
-    <p>
-      Follow <i class="fa fa-twitter"></i>
-      <a href="https://twitter.com/coli_conc" title="follow @coli_conc at twitter">@coli_conc</a>
-      at Twitter for project updates and news.
-    </p>
-  </div>
-</div>
+### Team
 
-<div class="row">
-  <div class="col-md-12">
-    <iframe style="border: 0; height: 240px; width: 100%;" scrolling="no" src="//piwik.gbv.de/index.php?module=CoreAdminHome&amp;action=optOut&amp;language=en"></iframe>
-  </div>
-</div>
+**General Contact**<br>
+coli-conc@gbv.de
 
-<?php
-include "$BASE/footer.php";
+**Project Coordination**<br>
+Uma Balakrishnan (balakrishnan@gbv.de)
+
+**Technical Coordination**<br>
+Dr. Jakob Voß (voss@gbv.de)
+
+**Software Development**<br>
+Stefan Peters (peters@gbv.de)
+
+**Student Assistant**<br>
+Dennis Tobola
+
+
+### News
+
+Follow [@coli_conc](https://twitter.com/coli_conc) at Twitter for project updates and news.
+
+
+<iframe style="border: 0; height: 240px; width: 100%;" scrolling="no" src="//piwik.gbv.de/index.php?module=CoreAdminHome&amp;action=optOut&amp;language=en"></iframe>
+
+
+
