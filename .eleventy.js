b4d981abb5c10d0c7f1a77781af9bf3e9ab62aa3
Stefan Peters <Stefan.Peters@gbv.de>
2020-07-13 09:47:20 +0200

    Adjust shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -31,19 +31,24 @@ module.exports = eleventyConfig => {
   })
 
   // Paired Shortcode for flex element
-  eleventyConfig.addPairedShortcode("flex", (content, flex = "1") => {
+  eleventyConfig.addPairedShortcode("flex", (content, flex = "1", style = "") => {
     return `
-<div class="flex" style="flex: ${flex};">
+<div class="flex" style="flex: ${flex}; ${style}">
   ${markdownIt.render(content)}
 </div>
     `
   })
+
+  eleventyConfig.addShortcode("flexBreakRow", () => `<div style="flex-basis: 100%; height: 0;"></div>`)
 
   eleventyConfig.addShortcode("icon", (name, width = "20px") => {
     return {
       user: `<svg aria-hidden="true" width="${width}" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>`,
     }[name] || ""
   })
+
+  // Shortcode for images
+  eleventyConfig.addShortcode("image", (url, style) => `<img src="${eleventyConfig.getFilter("urla")(url)}" style="${style}">`)
 
   // Paired Shortcode for Markdown
   eleventyConfig.addPairedShortcode("markdown", content => markdownIt.render(content))
