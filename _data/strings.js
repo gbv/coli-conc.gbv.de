f9ef67118f2abdd21dd055f57c61196ee7efce6c
Stefan Peters <Stefan.Peters@gbv.de>
2020-07-15 08:05:52 +0200

    Add dropdown menu on services

--- _data/strings.js
+++ _data/strings.js
@@ -6,6 +6,22 @@ module.exports = {
     services: {
       en: "Services",
       de: "Dienste",
+      kosTypes: {
+        en: "KOS Types",
+        de: "KOS-Typen",
+      },
+      kosRegistry: {
+        en: "KOS Registry",
+        de: "KOS-Register",
+      },
+      concordanceRegistry: {
+        en: "Concordance Registry",
+        de: "Konkordanz-Register",
+      },
+      mappingsApi: {
+        en: "Mappings API",
+        de: "Mapping-API",
+      },
     },
     partners: {
       en: "Partners",
