84d7f71518dca1aba39ebcd9fd8fdfabbe174916
Stefan Peters <Stefan.Peters@gbv.de>
2020-07-17 08:08:22 +0200

    Adjust resize-image to work for multiple files

--- scripts/resize-image.js
+++ scripts/resize-image.js
@@ -4,7 +4,7 @@ const meow = require('meow')
 
 const cli = meow(`
   Usage
-    $ ./resize-image.js <input file>
+    $ ./resize-image.js <input file> [<input file>, ...]
 
   Options
     --widths target widths in pixels separated by comma
@@ -20,19 +20,21 @@ const cli = meow(`
   }
 })
 
-const filename = cli.input[0]
-const fileExtension = filename.slice(filename.lastIndexOf("."))
 const widths = cli.flags.widths.split(",").map(w => parseInt(w))
 
 const sharp = require("sharp")
 
 ;(async () => {
-  for (let width of widths) {
-    const targetFilename = filename.replace(fileExtension, `-${width}px${fileExtension}`)
-    console.log(`Writing version with ${width} pixels width to ${targetFilename}...`)
-    await sharp(filename)
-      .resize(width)
-      .toFile(targetFilename)
-    console.log("...done!")
+  for (let filename of cli.input) {
+    const fileExtension = filename.slice(filename.lastIndexOf("."))
+    for (let width of widths) {
+      const targetFilename = filename.replace(fileExtension, `-${width}px${fileExtension}`)
+      console.log(`Writing version with ${width} pixels width to ${targetFilename}...`)
+      await sharp(filename)
+        .resize(width)
+        .toFile(targetFilename)
+      console.log("...done!")
+    }
+    console.log()
   }
 })()
