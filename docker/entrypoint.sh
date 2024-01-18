c743403bfba84523a4e4de389154e3430bfc436f
Stefan Peters <Stefan.Peters@gbv.de>
2024-01-18 13:54:47 +0100

    Docker: Use port 80 by default

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,7 @@
 # Build site in background
 bash build.sh &
 
-http-server -s -d false /usr/src/app/_site
+PORT=${PORT:-80}
+echo "Starting http-server on port $PORT..."
+
+http-server -s -d false -p "$PORT" /usr/src/app/_site
