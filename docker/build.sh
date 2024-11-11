d084dfbec29d99800ab0a7d115f06fe158dbf922
Stefan Peters <Stefan.Peters@gbv.de>
2024-11-11 14:15:14 +0100

    Improve Docker build script to prevent future issues

--- docker/build.sh
+++ docker/build.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
+REPO=coli-conc.gbv.de
+TARGET=../_site
+
 # Clone site if not yet done
-if [ ! -e .git ]; then
-  git init
-  git remote add origin https://github.com/gbv/coli-conc.gbv.de.git
-  git fetch
-  git checkout -t origin/main
+if [ ! -e $REPO/.git ]; then
+  git clone https://github.com/gbv/coli-conc.gbv.de.git $REPO
 fi
+
+cd $REPO || exit 1
+
+# Clean/reset repo
+git clean -f -d
+rm -rf _site
 
 # Pull changes
 git pull
 
-if [ -e _site/.git-commit ] && [ "$(git rev-parse HEAD)" == "$(cat _site/.git-commit)" ]; then
+if [ -e $TARGET/.git-commit ] && [ "$(git rev-parse HEAD)" == "$(cat $TARGET/.git-commit)" ]; then
   echo "Site rebuild skipped because there was no update."
 else
   # Might need to update dependencies after each pull
   npm ci
   # Build the site
-  npm run build -- --pathprefix="$PATHPREFIX" --url="$URL"
-  # Remember the current commit
-  git rev-parse HEAD > _site/.git-commit
+  if npm run build -- --pathprefix="$PATHPREFIX" --url="$URL"; then
+    # Copy to home folder
+    rm -rf $TARGET/*
+    cp -r _site/* $TARGET
+    # Remember the current commit
+    git rev-parse HEAD > $TARGET/.git-commit
+  else
+    echo "Warning: Build was not successful! Please check the logs."
+  fi
 fi
-
